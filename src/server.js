c299c68044bf85055d10dd43501f3478039dce92
thomazzzw <takeothomaz@gmail.com>
2026-09-08 18:14:08 -0300

    segundo commit

--- src/server.js
+++ src/server.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
     try{
     const{ nome, email } = req.body;
 
     const { result } = await pool.query(
-        "INSERT INTO usuarios VALUE (?, ?)",
+        "INSERT INTO usuarios (nome, email) VALUE (?, ?)",
         [nome, email]
     );
 
@@ -24,4 +24,56 @@ app.post('/', async (req, res) => {
     } catch(e) {
         res.status(500).json({ erro: "falha ao criar usuario"})
     }
+});
+
+app.get("/", async (req, res) =>{
+    try{
+        const [rows] = await pool.query("SELECT * FROM usuarios");
+        res.json(rows);
+    } catch (e) {
+        res.status(500).json({ erro: "Falha ao listar usuários"});
+    }
+});
+
+app.put("/id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { nome, email } = req.body;
+
+        const [result] = await pool.query(
+            "UPDATE usuarios SET nome = COALESCE(?, nome), email = COALESCE(?, email) WHERE id = ?", 
+            [nome || null, email || null, id]
+        );
+
+        if(!result.affectedRows) {
+            return res.status(404).json({ erro: "Usuário não encontrado"});
+        }
+
+        res.json({ mensagem: "atualizado com sucesso"});
+    } catch (e) {
+        res.status(500).json({ erro: "falha aoa atualizar usuario"})
+    }
+});
+
+app.delete("/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+
+        const [result] = await pool.query(
+            "DELETE FROM usuarios WHERE id = ?",
+            [id]
+        );
+
+        if(!result.affectedRows) {
+            return res.status(404).json({ erro: "Usuario não encontrado"});
+        }
+
+        res.json({ mensagem: "Deletado com sucesso"});
+    } catch (e) {
+        res.status(500).json({ erro: "Falha ao deletar usuario"});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor MySQL rodando em http://localhost:${PORT}`);
 })
